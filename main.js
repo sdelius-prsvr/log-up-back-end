52aa55efc299a70d44c0f85ab802573376890e32
stephen <sdelius.prsvr@gmail.com>
2023-10-02 11:42:17 -0500

    updated passwordMatches to use switch, commented out big Boolean return

--- main.js
+++ main.js
@@ -33,9 +33,13 @@ function isRegisteredUser(username){
 }
 
 function passwordMatches(username, password){
-  return (username === user1 && password === password1)
-      || (username === user2 && password === password2)
-      || (username === user3 && password === password3);
+  // return (username === user1 && password === password1)
+  //     || (username === user2 && password === password2)
+  //     || (username === user3 && password === password3);
+  switch(username){
+    case user1: return password === password1
+    case user2: return password === password2
+    case user3: return password === password3}
 }
 
 
